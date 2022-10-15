43cdaad112665ec7481f78eb1b78d3736f70126c
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 12:05:39 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-/*
- * main- use printf to print
- * specific output
+/**
+ * main - Entry point
+ *
+ * 
+ *
+ * Description: 'print using printf'
+ *
+ * 
+ *
+ *  Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
