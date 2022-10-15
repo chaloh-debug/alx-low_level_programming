895fe8cc5d1e784471ac46fd15e82a7cca777226
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 11:59:20 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main- use printf to print
+ * specific output
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
