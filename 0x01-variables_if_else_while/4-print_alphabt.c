0aa4fc8e7d4ed0115c353a57505e7804e0d35192
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 15:55:36 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,12 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z';i !='q,e')
+	for (i = 'a'; i <= 'z';i++')
 	{
-		putchar(i);
-		i++;
+		if (i != e && i != q)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
