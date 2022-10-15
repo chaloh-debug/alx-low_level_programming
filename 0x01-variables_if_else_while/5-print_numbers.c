e365552aff7ba1934aace5ac83dd6487f12e7d6f
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 16:09:07 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Start point
+ *
+ * print numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i=0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
