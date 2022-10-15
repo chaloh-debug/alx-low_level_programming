ee59bd91bd8c1e69a94d8a288110c024a7f41800
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 14:47:12 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Start point
+ *
+ * alphabet
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+	       putchar(i);
+	}
+	return (0);
+}
