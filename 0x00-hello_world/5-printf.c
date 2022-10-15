1d03df4cd02e90fc57bb82b10b602083f11217a4
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 12:09:28 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * 
  *
- * Description: 'print using printf'
  *
- * 
+ * print using printf
+ *
+ *
  *
  *  Return: Always 0 (Success)
  *
@@ -15,5 +15,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
