2c21c6f9f8a8230454c8c7e2d124aac7664fe73c
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 15:27:30 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- start
+ *
+ * Aa TO Zz
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i='a', i<='z', i++)
+	{
+		putchar(i);
+
+		for (i='A', i='Z',i++)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
