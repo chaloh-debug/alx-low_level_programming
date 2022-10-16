c08aa2067837df97b6fb5b3df6b3f55e0e76635d
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 17:13:59 -0700

    nest

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- Start point
+ *
+ * nested loop
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(j);
+			putchar(", ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
