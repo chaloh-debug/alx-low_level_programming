cd9d6f0e05a4866660dfd0825093c7971aea74a8
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-15 17:44:32 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Start point
+ *
+ * stderr
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
