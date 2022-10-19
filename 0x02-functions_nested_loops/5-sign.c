48b54f3c0ba838199203a54ad74f8ead63af2cfd
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-18 19:41:10 -0700

    nesting

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  *
- * @n ASCII character
+ * @n: ASCII character
  *
  * return: 0 if zero. 1 if greater than zero
  * -1 if less than zero.
@@ -18,11 +18,11 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n = 0)
-	{ 
+	{
 		_putchar(0);
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar(45);
 		return (-1);
