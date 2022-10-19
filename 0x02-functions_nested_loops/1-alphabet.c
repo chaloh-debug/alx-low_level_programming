d4430fdb0bbe52940bae20b74dfab08aa746cf82
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-18 17:13:37 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main- Start point
+ *
+ * function prototypes
+ *
+ * Return 0 (Success)
+ */
+
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; i <= 122; i++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n);
+}
