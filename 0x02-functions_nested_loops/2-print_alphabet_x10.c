b8911b758e55de47348b4dc8d6bd4fffaa20f558
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-18 17:33:37 -0700

    nesting

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main- START
+ *
+ * nested loop
+ *
+ * Return: 0 (sucess)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 97;j <= 122; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n');
+	}
+}
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
