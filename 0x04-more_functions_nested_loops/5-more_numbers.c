1a116098ef4d26970006a6a8c42cc2a20c65a281
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-21 14:32:18 -0700

    print 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14
+ * (10 times)
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i, c;
+
+	for (i = 0; i < 10; i+)
+	{
+		for(c = '0'; c <= '14')
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
