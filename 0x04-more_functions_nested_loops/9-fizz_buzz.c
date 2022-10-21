23f7d32fad1790dbfc2516adfa78ab53008a1893
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-21 16:41:05 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,27 +8,29 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
