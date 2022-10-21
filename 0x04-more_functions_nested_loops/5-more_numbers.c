22357eed4f068a91c6d5a4590cac6410958ca5a9
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-21 16:54:43 -0700

    0 to 14, 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,13 @@ void more_numbers(void)
 {
 	int i, c;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(c = 0; c <= 14; c++)
+		for(c = 0; c < 15; c++)
 		{
-			if (c > 9)
-			{
-				_putchar((c / 10) + '0');
-			}
-			_putchar((c % 10) + '0');
+			if (c >= 10)
+				_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
 		}
 		_putchar('\n');
 	}
