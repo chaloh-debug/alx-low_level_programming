cff37c0057cd3915fc90377f1fd6e02c4dc0beab
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-25 17:08:54 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,16 @@
 
 void print_rev(char *s)
 {
-	int count;
-	
+	int count = 0;
+
 	while (*(s + count) != '\0')
 	{
 		count++;
 	}
-	while (count >= 0)
+	while (count > 0)
 	{
-		_putchar(*(s + (count)));
+		_putchar(*(s + count));
 		count--;
-	} _putchar('\n');
+	}
+	_putchar('\n');
 }
