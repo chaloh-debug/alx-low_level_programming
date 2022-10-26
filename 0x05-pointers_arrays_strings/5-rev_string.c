5a76e34963496c1ba23e01cb6f7de3266360de7b
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-25 17:40:29 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char str[];
+
+	i = 0;
+	j = 0;
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+
+	while (i >= 0)
+	{
+		*(s + i) = *(s + j);
+		j++;
+		i--;
+	}
+}
