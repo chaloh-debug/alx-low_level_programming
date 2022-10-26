6125b64d0ff4ddb0fd15b30949c59e5478157586
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-10-25 19:17:16 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array.
+ * @a: input
+ * @n: input
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int c = 0;
+
+	for (c = 0; c < n; c++)
+	{
+		printf("%d", *(a + i));
+
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
