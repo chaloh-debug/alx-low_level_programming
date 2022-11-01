1c6b742b2225e62e8b03cf7f8b34e475aadf6480
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-11-01 15:39:27 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,8 +13,11 @@ char *_strpbrk(char *s, char *accept)
 	
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (*(s + i) == *(accept + i))
-			return (s + i);
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + i))
+				return (s + i);
+		}
 	}
 	return ('\0');
 }
