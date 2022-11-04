2126133b19d977e72560fe73d2634e131f7b8fbb
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-11-03 18:48:29 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * division - trial division
+ * @n: input
+ * @i: divider
+ * Return: 0
+ */
+
+int division(int n, int i)
+{
+	if (n == i)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else
+		return (division(n, i + 1));
+}
+/**
+ * is_prime_number - looks for prime number.
+ * @n: input integer
+ * Return: 0
+ */
+
+int is_prime_number(int n)
+{
+	if (i <= 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (division(n, 2));
+}
