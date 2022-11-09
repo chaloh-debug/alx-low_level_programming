e840b5af83f254dd13ad8fd82e3d0b6642c14a19
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-11-09 11:42:21 -0800

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,13 +15,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr;
 
-	ptr = malloc(size * sizeof(c));
-	if (size != 0)
-	{
-		for (i = 0; i < size; i++)
-			ptr[i] = c;
-		return (ptr);
-	}
-	else
+	if (size == 0)
 		return (NULL);
+
+	ptr = malloc(size * sizeof(c));
+	if (ptr == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
 }
