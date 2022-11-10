a67c7c3497aaedac04353ee4f97064e35f5ef087
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-11-09 17:24:30 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,19 +14,19 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, s;
 	char *conct;
 
-	for (i = 0; s1 != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; s2 != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 	conct = (char *)malloc(sizeof(char) * (i + j + 1));
 
-	if (cont == NULL)
+	if (conct == NULL)
 		return (NULL);
 
 	for (s = 0; s <= i; s++)
 		conct[s] = s1[s];
 
-	for (k = 0; k <= j: s++, k++)
+	for (k = 0; k <= j; s++, k++)
 		conct[s] = s2[k];
 	return (conct);
 }
