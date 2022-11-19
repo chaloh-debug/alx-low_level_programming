2b9e42ec10d8d9bfcfe26a6d43b73d81293f8e59
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-11-19 13:39:52 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * prints a name.
+ * @name: pointer to name.
+ * @f: pointer to function.
+ * Return: nil
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	{
+		f = name;
+	}
+}
