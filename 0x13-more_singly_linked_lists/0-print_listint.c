a18cf177d9b64027e4865e61b168d27698663955
chaloh-debug <charlesgitahi.cn@gmail.com>
2022-12-01 10:04:52 -0800

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#includei "lists.h"
 /*
  * print_listint - prints all the elements of a listint_t list.
  * @h: list pointer
@@ -6,8 +6,7 @@
  */
 
 size_t print_listint(const listint_t *h)
-{
-	size_t prints = 0;
+
 	while (h! = NULL)
 	{
 		printf("%d\n", h->n);
