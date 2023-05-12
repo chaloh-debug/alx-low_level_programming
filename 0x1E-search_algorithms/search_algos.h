ff9cf057732d32d56a2901ee0e1e6a22b16a1485
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-13 02:23:47 +0300

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -44,6 +44,7 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+/*int binary_search_ex(int *array, int value, int first, int last);*/
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
